55ac0fb436ac4a29ccee7352bd3aa70d832e783e
JonghoKim-YonseiUniversity <jongho.kim@yonsei.ac.kr>
2019-03-25 01:20:08 +0000

    Example: Running MNIST

--- script/run_mnist.sh
+++ script/run_mnist.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+CAFFE_HOME=$HOME/caffe
+
+cd $CAFFE_HOME
+
 $CAFFE_HOME/build/tools/caffe test -model $CAFFE_HOME/examples/mnist/lenet_train_test.prototxt -weights $CAFFE_HOME/examples/mnist/lenet_iter_10000.caffemodel -gpu all
