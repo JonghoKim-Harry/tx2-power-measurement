363fa3f6ee91c4ac3920939e933d08cb529e7758
JonghoKim-YonseiUniversity <jongho.kim@yonsei.ac.kr>
2019-04-24 04:40:18 +0000

    New script: run_caffenet.sh

---
+++ script/run/run_caffenet.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+INPUT_IMAGE=$1
+
+$CAFFE_ROOT/build/examples/cpp_classification/classification.bin $CAFFE_ROOT/models/bvlc_reference_caffenet/deploy.prototxt $CAFFE_ROOT/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel $CAFFE_ROOT/data/ilsvrc12/imagenet_mean.binaryproto $CAFFE_ROOT/data/ilsvrc12/synset_words.txt $INPUT_IMAGE
