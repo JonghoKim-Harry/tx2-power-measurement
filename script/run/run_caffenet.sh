f5c295468bd5cd4a8c23f59f2e3c8e5e59085e6b
JonghoKim-YonseiUniversity <jongho.kim@yonsei.ac.kr>
2019-04-24 04:56:47 +0000

    Fix and more examples in README

--- script/run/run_caffenet.sh
+++ script/run/run_caffenet.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export CAFFE_ROOT=/home/nvidia/caffe
+
 INPUT_IMAGE=$1
 
 $CAFFE_ROOT/build/examples/cpp_classification/classification.bin $CAFFE_ROOT/models/bvlc_reference_caffenet/deploy.prototxt $CAFFE_ROOT/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel $CAFFE_ROOT/data/ilsvrc12/imagenet_mean.binaryproto $CAFFE_ROOT/data/ilsvrc12/synset_words.txt $INPUT_IMAGE
