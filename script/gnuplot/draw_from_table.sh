ab7ac1c229d97c91e884645d3d87dedf932ed8d6
JonghoKim-YonseiUniversity <jongho.kim@yonsei.ac.kr>
2019-04-28 14:04:27 +0000

    Argument processing w/ shell scripts + Plotting w/ gnuplot

---
+++ script/gnuplot/draw_from_table.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+#
+# If you don't know shift command of shell, see:
+# https://tiswww.case.edu/php/chet/bash/bashref.html#Bourne-Shell-Builtins
+#
+
+usage() {
+
+    echo "Usage: $0 [options] <column index> <column name>..."
+    echo ""
+    echo "Options:"
+    echo "   -h, --help               Print usage"
+    echo "   -i, --input-file         Input file"
+    echo "   -o, --output-file        Output file"
+    echo "   -t, --title              Title of the chart"
+    echo "   -x, --x-axis             Index of x-axis"
+}
+
+COLUMN=()
+COLUMN_IDX=
+COLUMN_NAME=
+SKIP=
+        
+for opt in "$@"; do
+    case $opt in
+        -h|--help)
+            usage
+            exit
+            ;;
+
+        -i|--input-file)
+            INPUT_FILE=$2
+            SKIP=YES
+            shift 2
+            ;;
+
+        -o|--output-file)
+            OUTPUT_FILE=$2
+            SKIP=YES
+            shift 2
+            ;;
+ 
+        -t|--title)
+            TITLE=$2
+            SKIP=YES
+            shift 2
+            ;;
+
+        -x|--x-axis)
+            X_AXIS_IDX=$2
+            SKIP=YES
+            shift 2
+            ;;
+
+        -*)
+            echo "Unknown Option: $opt"
+            if [[ "$#" -gt 0 ]]; then shift; fi
+            ;;
+
+        *)
+            if [[ -z "$SKIP" ]]; then
+                COLUMN_IDX=$1
+                COLUMN_NAME=$2
+                COLUMN+=(":$COLUMN_IDX title \"$COLUMN_NAME\"")
+                shift 2
+                SKIP=YES
+            else
+                SKIP=
+            fi
+            ;;
+    esac
+done
+
+        
+#echo "INPUT_FILE: $INPUT_FILE"
+#echo "OUTPUT_FILE: $OUTPUT_FILE"
+#echo "TITLE: $TITLE"
+#echo "X-AXIS INDEX: $X_AXIS_IDX"
+
+
+# You should give input file
+if [[ -z "$INPUT_FILE" ]]; then
+    echo "You should give us input file"
+    usage
+    exit
+fi
+
+# You should give output file
+if [[ -z "$OUTPUT_FILE" ]]; then
+    echo "You should give us output file"
+    usage
+    exit
+fi
+
+# Default x-axis index is 2
+if [[ -z "$X_AXIS_IDX" ]]; then
+    X_AXIS_IDX="2"
+fi
+
+        
+GNUPLOT_CMD=
+exec < $(dirname $0)/setup.gnuplot
+while read LINE; do
+    GNUPLOT_LINE=$(echo $LINE | sed -rn '/[[:space:]]*#.*/!p')
+    if [[ -n "$GNUPLOT_LINE" ]]; then
+        echo $GNUPLOT_LINE
+    fi
+done
+
+
+GNUPLOT_CMD="$GNUPLOT_CMD\nset output $OUTPUT_FILE"
+GNUPLOT_CMD="$GNUPLOT_CMD\nset title $TITLE"
+
+PLOT=
+for COL in "${COLUMN[@]}"; do
+    if [[ -z "$PLOT" ]]; then
+        GNUPLOT_CMD="$GNUPLOT_CMD\nplot $INPUT_FILE using $X_AXIS_IDX$COL"
+        PLOT=YES
+    else
+        GNUPLOT_CMD="$GNUPLOT_CMD\nreplot $INPUT_FILE using $X_AXIS_IDX$COL"
+    fi
+done
+
+echo -e $GNUPLOT_CMD
+#TODO: gnuplot -c $GNUPLOT_CMD
