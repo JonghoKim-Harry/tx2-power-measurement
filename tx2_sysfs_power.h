7e980589e809dd0bae912d9c2bad34e549779aef
JonghoKim-YonseiUniversity <jongho.kim@yonsei.ac.kr>
2019-05-06 05:24:03 +0000

    TX2 SYSFS STATS using strace

--- tx2_sysfs_power.h
+++ tx2_sysfs_power.h
@@ -13,6 +13,24 @@
 #define TX2_SYSFS_CPU_FREQ_MAX_STRLEN 7    // kHz
 #define TX2_SYSFS_CPU_POWER_MAX_STRLEN 5   // mW
 
+// Thermal Informations
+#define TX2_SYSFS_BCPU_THERM     \
+    "/sys/devices/virtual/thermal/thermal_zone0/temp"
+#define TX2_SYSFS_MCPU_THERM     \
+    "/sys/devices/virtual/thermal/thermal_zone1/temp"
+#define TX2_SYSFS_GPU_THERM      \
+    "/sys/devices/virtual/thermal/thermal_zone2/temp"
+#define TX2_SYSFS_PLL_THERM      \
+    "/sys/devices/virtual/thermal/thermal_zone3/temp"
+#define TX2_SYSFS_TBOARD_THERM   \
+    "/sys/devices/virtual/thermal/thermal_zone4/temp"
+#define TX2_SYSFS_TDIODE_THERM   \
+    "/sys/devices/virtual/thermal/thermal_zone5/temp"
+#define TX2_SYSFS_PMIC_THERM     \
+    "/sys/devices/virtual/thermal/thermal_zone6/temp"
+#define TX2_SYSFS_FAN_THERM      \
+    "/sys/devices/virtual/thermal/thermal_zone7/temp"
+
 /* Power Measurement I */
 #define TX2_SYSFS_POWER_GPU    \
     "/sys/devices/3160000.i2c/i2c-0/0-0040/iio_device/in_power0_input"
@@ -74,7 +92,12 @@
     "/sys/kernel/debug/bpmp/debug/clk/emc/max_rate"
 #define TX2_SYSFS_EMC_MINFREQ   \
     "/sys/kernel/debug/bpmp/debug/clk/emc/min_rate"
-#define TX2_SYSFS_EMC_FREQ   \
+#define TX2_SYSFS_EMC_FREQ      \
     "/sys/kernel/debug/clk/emc/clk_rate"
+#define TX2_SYSFS_EMC_UTIL_MAYBE   \
+    "/sys/kernel/actmon_avg_activity/mc_all"
+
+// APE (Audio Processing Engine)
+#define TX2_SYSFS_APE_FREQ   "/sys/kernel/debug/clk/ape/clk_rate"
 
 #endif   // TX2_SYSFS_POWER_H
