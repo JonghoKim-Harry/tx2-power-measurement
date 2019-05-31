521d700ab4acdc00c988f61035be473ce9b67542
Jongho Kim <scurry2119@gmail.com>
2019-05-31 17:43:42 +0900

    CAFFE_ROOT: /home/nvidia/caffe -> /home/jongho/caffe

--- script/run/run_cifar10.sh
+++ script/run/run_cifar10.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export CAFFE_ROOT=/home/nvidia/caffe
+export CAFFE_ROOT=$HOME/caffe
 cd $CAFFE_ROOT
 
 $CAFFE_ROOT/build/tools/caffe test -model $CAFFE_ROOT/examples/cifar10/cifar10_full_train_test.prototxt -weights $CAFFE_ROOT/examples/cifar10/cifar10_full_iter_70000.caffemodel.h5 -gpu all
--- script/run/run_mnist.sh
+++ script/run/run_mnist.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CAFFE_ROOT=/home/nvidia/caffe
+export CAFFE_ROOT=$HOME/caffe
 cd $CAFFE_ROOT
 
 $CAFFE_ROOT/build/tools/caffe test -model $CAFFE_ROOT/examples/mnist/lenet_train_test.prototxt -weights $CAFFE_ROOT/examples/mnist/lenet_iter_10000.caffemodel -gpu all
