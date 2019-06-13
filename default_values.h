343df6c1443a62c762c0f4d0675af47140bbaa59
Jongho Kim <scurry2119@gmail.com>
2019-06-13 16:48:47 +0900

    * MAX_COLWIDTH to 80 for caffelogs * Move default values: measurement_info.h -> default_values.h

--- default_values.h
+++ default_values.h
@@ -9,4 +9,24 @@
 
 #define MAX_BUFFLEN   256
 
+// sysfs fds
+#define MAX_NUM_SYSFS_FD     8
+#define  NO_SYSFS_FILE       0
+#define ONE_SYSFS_FILE       1
+#define TWO_SYSFS_FILES      2
+#define THREE_SYSFS_FILES    3
+#define FOUR_SYSFS_FILES     4
+#define FIVE_SYSFS_FILES     5
+#define SIX_SYSFS_FILES      6
+#define SEVEN_SYSFS_FILES    7
+#define EIGHT_SYSFS_FILES    8
+
+#define MAX_NUM_RAWDATA     20
+#define MAX_NUM_STAT        20
+#define RAWDATA_BUFFSIZE    64
+#define MAX_COLWIDTH        80
+
+#define NUM_CPUS             6
+
+
 #endif   // DEFAULT_VALUES_H
--- measurement_info.h
+++ measurement_info.h
@@ -7,26 +7,8 @@
 #include <stdarg.h>
 #include <regex.h>
 
+#include "default_values.h"
 #include "list.h"
-
-// sysfs fds
-#define MAX_NUM_SYSFS_FD     8
-#define  NO_SYSFS_FILE       0
-#define ONE_SYSFS_FILE       1
-#define TWO_SYSFS_FILES      2
-#define THREE_SYSFS_FILES    3
-#define FOUR_SYSFS_FILES     4
-#define FIVE_SYSFS_FILES     5
-#define SIX_SYSFS_FILES      6
-#define SEVEN_SYSFS_FILES    7
-#define EIGHT_SYSFS_FILES    8
-
-#define MAX_NUM_RAWDATA     20
-#define MAX_NUM_STAT        20
-#define RAWDATA_BUFFSIZE    64
-#define MAX_COLWIDTH        40
-
-#define NUM_CPUS             6
 
 struct rawdata_info_struct;
 struct powerlog_struct;
