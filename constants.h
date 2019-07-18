13f4a53ed39eec29b1156b16da1e1a6f8ff88bbe
Jongho Kim <scurry2119@gmail.com>
2019-07-18 19:04:00 +0900

    KILO, MEGA, GIGA

--- constants.h
+++ constants.h
@@ -1,6 +1,9 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+#define       KILO                                     1000
+#define       MEGA                                  1000000
+#define       GIGA                               1000000000
 #define       ONE_PER_MILLI                            1000
 #define       ONE_PER_MICRO                         1000000
 #define       ONE_PER_NANO                       1000000000
