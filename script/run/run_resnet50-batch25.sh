e1e195ac1d91bfc27aa2359dab528944df272dae
Jongho Kim <scurry2119@gmail.com>
2019-08-13 15:36:58 +0900

    Add ResNet: ResNet-50, ResNet-101, ResNet-152

---
+++ script/run/run_resnet50-batch25.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export CAFFE_ROOT=$HOME/caffe
+cd $CAFFE_ROOT
+
+NETWORK_PATH=$CAFFE_ROOT/models/modelzoo_resnet
+PROTO=$NETWORK_PATH/ResNet-50-train_val.prototxt
+WEIGHTS=$NETWORK_PATH/ResNet-50-model.caffemodel
+
+COMMAND="$CAFFE_ROOT/build/tools/caffe test -model $PROTO  -weights $WEIGHTS -gpu all"
+
+echo "$0: $COMMAND"
+$COMMAND
