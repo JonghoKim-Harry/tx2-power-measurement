da797a1eb5e8670be19d3351b45f0ffc24e75853
Jongho Kim <scurry2119@gmail.com>
2019-08-14 10:40:07 +0900

    Now we can set MIN/MAX GPU frequency

--- script/run/set_gpu_freq.sh
+++ script/run/set_gpu_freq.sh
@@ -3,7 +3,10 @@
 #
 # This script set GPU frequency to be fixed to given frequency value.
 # 
-#     $1: Frequency
+#     $1: Min Frequency
+#     $2: (Max Frequency)
+#
+#   * If maximum frequency is not given, it will be equal to minimum frequency
 #
 #   * Note that given frequency is not among available frequency,
 #     it will be denied with error message
@@ -12,34 +15,49 @@
 #     it will be denied with error message
 #
 
-FIXED_GPU_FREQ=$1
+MIN_GPU_FREQ=$1
+
+MAX_GPU_FREQ=
+if [ "$#" -gt 1 ]; then
+    MAX_GPU_FREQ=$2
+else
+    MAX_GPU_FREQ=$MIN_GPU_FREQ
+fi
+
 AVAILABLE_GPU_FREQ_LIST=$(cat /sys/devices/17000000.gp10b/devfreq/17000000.gp10b/available_frequencies)
 
-FOUND=0
+MIN_FREQ_FOUND=0
+MAX_FREQ_FOUND=0
 
 #
 # You should run this script with sudoer privilege
 #
 if [ "$EUID" -ne 0 ]; then
-    echo "Please run as root privilege"
+    echo "$0: Please run as root privilege"
     exit
 fi
 
 for AVAILABLE_FREQ in $AVAILABLE_GPU_FREQ_LIST
 do
-    if [ "$FIXED_GPU_FREQ" == "$AVAILABLE_FREQ" ]
+    if [ "$MIN_GPU_FREQ" == "$AVAILABLE_FREQ" ]
     then
-        FOUND=1
-        break
+        MIN_FREQ_FOUND=1
+    fi
+    if [ "$MAX_GPU_FREQ" == "$AVAILABLE_FREQ" ]
+    then
+        MAX_FREQ_FOUND=1
     fi
 done;
 
-if [ "$FOUND" == "0" ]; then
-    echo "ERROR: You gave unavailable frequency"
+if [ "$MIN_FREQ_FOUND" == "0" ]; then
+    echo "$0: ERROR: You gave unavailable minimum frequency"
+elif [ "$MAX_FREQ_FOUND" == "0" ]; then
+    echo "$0: ERROR: You gave unavailable maximum frequency"
 else
-    echo "You gave available frequency"
-    sudo echo $FIXED_GPU_FREQ > /sys/devices/17000000.gp10b/devfreq/17000000.gp10b/min_freq
-    sudo echo $FIXED_GPU_FREQ > /sys/devices/17000000.gp10b/devfreq/17000000.gp10b/max_freq
-    echo "GPU frequency is set to $FIXED_GPU_FREQ Hz"
+    sudo echo $MIN_GPU_FREQ > /sys/devices/17000000.gp10b/devfreq/17000000.gp10b/min_freq
+    echo "$0: Minimum GPU frequency is set to $MIN_GPU_FREQ Hz"
+     
+    sudo echo $MAX_GPU_FREQ > /sys/devices/17000000.gp10b/devfreq/17000000.gp10b/max_freq
+    echo "$0: Maximum GPU frequency is set to $MAX_GPU_FREQ Hz"
 fi
 
