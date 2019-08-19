77076b3c91d9928afa7517efe63d56dd67f27651
Jongho Kim <scurry2119@gmail.com>
2019-08-19 18:54:23 +0900

    Add VGGNet-16

---
+++ script/run/run_vggnet16-batch50.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export CAFFE_ROOT=$HOME/caffe
+cd $CAFFE_ROOT
+
+NETWORK_PATH=$CAFFE_ROOT/models/modelzoo_vggnet
+PROTO=$NETWORK_PATH/VGG_ILSVRC_16_layers_train_val.prototxt
+WEIGHTS=$NETWORK_PATH/VGG_ILSVRC_16_layers.caffemodel
+
+COMMAND="$CAFFE_ROOT/build/tools/caffe test -model $PROTO  -weights $WEIGHTS -gpu all"
+
+echo "$0: $COMMAND"
+$COMMAND
